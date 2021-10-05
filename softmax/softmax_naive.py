7b9af53e2e16e3ed3738e5970f7316cbb2fa6ba2
Abdul Dakkak <adakkak@microsoft.com>
2021-10-05 15:54:48 -0700

    .

--- softmax/softmax_naive.py
+++ softmax/softmax_naive.py
@@ -4,25 +4,26 @@ import math
 import robopy as rp
 
 N = 2**20
+DEV_MODE = False
 
 Input = rp.Array(role=rp.Array.Role.INPUT, element_type=rp.ScalarType.float32, shape=(N,))
 Output = rp.Array(role=rp.Array.Role.INPUT_OUTPUT, element_type=rp.ScalarType.float32, shape=(N,))
 Denom = rp.Array(role=rp.Array.Role.TEMP, element_type=rp.ScalarType.float32, shape=(1,))
 MaxVal = rp.Array(role=rp.Array.Role.TEMP, element_type=rp.ScalarType.float32, shape=(1,))
 
-zero_nest = rp.Nest(shape=(1,))
-z = zero_nest.get_indices()
-@zero_nest.iteration_logic
+init_nest = rp.Nest(shape=(1,))
+z = init_nest.get_indices()
+@init_nest.iteration_logic
 def _():
-    MaxVal[z] = -math.inf
+    MaxVal[z] = -math.inf 
     Denom[z] = 0.0
-zero_schedule = zero_nest.create_schedule()
+init_schedule = init_nest.create_schedule()
 
 max_nest = rp.Nest(shape=(N,))
-i = max_nest.get_indices()
+m = max_nest.get_indices()
 @max_nest.iteration_logic
 def _():
-    MaxVal[i] = rp.max(MaxVal[i], Input[i])
+    MaxVal[0] = rp.max(MaxVal[0], Input[m])
 max_schedule = max_nest.create_schedule()
 
 exp_nest = rp.Nest(shape=(N,))
@@ -33,16 +34,15 @@ def _():
     Denom[0] += Output[i]
 exp_schedule = exp_nest.create_schedule()
 
-finalize_nest = rp.Nest(shape=(N, ))
-j = finalize_nest.get_indices()
-@finalize_nest.iteration_logic
+div_nest = rp.Nest(shape=(N, ))
+j = div_nest.get_indices()
+@div_nest.iteration_logic
 def _():
-    Output[j] = Output[j] / Denom[0]
-final_schedule = finalize_nest.create_schedule()
+    Output[j] /= Denom[0]
+div_schedule = div_nest.create_schedule()
 
-fused_schedule = rp.fuse((zero_schedule, max_schedule, exp_schedule, final_schedule), partial=0)
-f, z, i, j, k = fused_schedule.get_indices()
-
+fused_schedule = rp.fuse((init_schedule, max_schedule, exp_schedule), partial=0)
+fused_schedule = rp.fuse((fused_schedule, div_schedule), partial=0)
 
 fused_plan = fused_schedule.create_action_plan()
 
@@ -51,4 +51,4 @@ target = rp.Target(category=rp.Target.Category.CPU)
 package = rp.Package()
 package.add_function(fused_plan, args=(Output, Input), base_name="softmax_naive")
 
-package.build(name="softmax_naive", format=rp.Package.Format.HAT)
+package.build(name="softmax_naive", format=rp.Package.Format.HAT, mode=rp.Package.Mode.DEBUG if DEV_MODE else rp.Package.Mode.RELEASE )
