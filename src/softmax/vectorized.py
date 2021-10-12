cd4cb7ef6caac3bf439c7000ddd34531432613d7
Abdul Dakkak <adakkak@microsoft.com>
2021-10-12 15:06:59 -0700

    checkpoint

--- src/softmax/vectorized.py
+++ src/softmax/vectorized.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 import math
 import robopy as acc
-# from robopy._lang_python import fast_exp
+from robopy._lang_python import fast_exp
 
 N = 2 ** 10
 DEV_MODE = False
 
 target = acc.Target(category=acc.Target.Category.CPU)
+vector_size = target.vector_bytes // 4
 
 Input = acc.Array(
     role=acc.Array.Role.INPUT, element_type=acc.ScalarType.float32, shape=(N,)
@@ -50,8 +51,8 @@ i = exp_nest.get_indices()
 
 @exp_nest.iteration_logic
 def _():
-    # Output[i] = fast_exp(Input[i] - MaxVal[0])
-    Output[i] = acc.exp(Input[i] - MaxVal[0])
+    Output[i] = fast_exp(Input[i] - MaxVal[0])
+    # Output[i] = acc.exp(Input[i] - MaxVal[0])
 
 
 exp_schedule = exp_nest.create_schedule()
@@ -87,19 +88,27 @@ f, f3, f1, z, m, f2, i, a, j = fused_schedule.get_indices()
 
 fused_plan = fused_schedule.create_action_plan()
 
-zz = fused_schedule.split(z, 2 * target.vector_bytes // 4)
-mm = fused_schedule.split(m, 2 * target.vector_bytes // 4)
-ii = fused_schedule.split(i, 2 * target.vector_bytes // 4)
-aa = fused_schedule.split(a, 2 * target.vector_bytes // 4)
-jj = fused_schedule.split(j, 2 * target.vector_bytes // 4)
+mm = fused_schedule.split(m, 8 * vector_size)
+ii = fused_schedule.split(i, 8 * vector_size)
+aa = fused_schedule.split(a, 8 * vector_size)
+jj = fused_schedule.split(j, 8 * vector_size)
 
-fused_schedule.reorder(f, f3, f1, z, zz, m, mm, f2, i, ii, a, aa, j, jj)
+mmm = fused_schedule.split(mm, 2 * vector_size)
+iii = fused_schedule.split(ii, 2 * vector_size)
+aaa = fused_schedule.split(aa, 2 * vector_size)
+jjj = fused_schedule.split(jj, 2 * vector_size)
 
-fused_plan.vectorize(zz)
-fused_plan.vectorize(mm)
-fused_plan.vectorize(ii)
-fused_plan.unroll(aa) # TODO: Add vectorization
-fused_plan.vectorize(jj)
+fused_schedule.reorder(f, f3, f1, z, m, mm, mmm, i, a, ii, aa, f2, iii, aaa, j, jj, jjj)
+
+fused_plan.unroll(mm)
+fused_plan.unroll(ii)
+fused_plan.unroll(aa)
+fused_plan.unroll(jj)
+
+fused_plan.vectorize(mmm)
+fused_plan.vectorize(iii)
+fused_plan.unroll(aaa)  # TODO: Add vectorization
+fused_plan.vectorize(jjj)
 
 
 package = acc.Package()
