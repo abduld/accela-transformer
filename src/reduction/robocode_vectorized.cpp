235280b5c24cbea061694a4db2a05dbc7d11700f
Abdul Dakkak <adakkak@microsoft.com>
2021-10-15 18:53:53 -0700

    checkpoint

--- src/reduction/robocode_vectorized.cpp
+++ src/reduction/robocode_vectorized.cpp
@@ -5,13 +5,14 @@
 #include "vectorized.hat"
 
 static constexpr int VectorSize = 8;
+static constexpr int SplitSize = 4 * VectorSize;
 
 static void BENCHMARK_NAME(Robocode_Vectorized)(benchmark::State& state) {
   aligned_vector<float> Input(N, 1.0/N);
   float output = 0;
   for (auto _ : state) {
     output = 0;
-    std::array<float, VectorSize> SumVec{0};
+    std::array<float, SplitSize> SumVec{0};
     vectorized(&output, Input.data(), SumVec.data());
     benchmark::DoNotOptimize(Input.data());
     benchmark::ClobberMemory();
--- src/reduction/vectorized.py
+++ src/reduction/vectorized.py
@@ -10,27 +10,28 @@ target = acc.Target(category=acc.Target.Category.CPU)
 vector_size = (
     target.vector_bytes // 4
 )  # AVX-2 gives 256-bit registers, which can hold 8 floats
+split_size = 4 * vector_size
 vector_units = 2 * vector_size  # AVX-2 has 16 256-bit registers
 
 Input = acc.Array(
     role=acc.Array.Role.INPUT, element_type=acc.ScalarType.float32, shape=(N,)
 )
 SumVec = acc.Array(
-    role=acc.Array.Role.TEMP, element_type=acc.ScalarType.float32, shape=(vector_size,)
+    role=acc.Array.Role.TEMP, element_type=acc.ScalarType.float32, shape=(split_size,)
 ) 
 Sum = acc.Array(
     role=acc.Array.Role.INPUT_OUTPUT, element_type=acc.ScalarType.float32, shape=(1,)
 ) 
  
-vec_accum_nest = acc.Nest(shape=(N // vector_size, vector_size))
+vec_accum_nest = acc.Nest(shape=(N // split_size, split_size))
 i, j = vec_accum_nest.get_indices()
 
 @vec_accum_nest.iteration_logic
 def _():
-    SumVec[j] += Input[i * vector_size + j]
+    SumVec[j] += Input[i * split_size + j]
 
  
-finalize_accum_nest = acc.Nest(shape=(vector_size,))
+finalize_accum_nest = acc.Nest(shape=(split_size,))
 k = finalize_accum_nest.get_indices()
 @finalize_accum_nest.iteration_logic
 def _():
@@ -44,7 +45,7 @@ f, i, j, k = fused_schedule.get_indices()
 
 ii = fused_schedule.split(i, vector_units)
 
-fused_schedule.reorder((f, i, ii, j, k))
+# fused_schedule.reorder((f, i, ii, j, k))
 
 fused_plan = fused_schedule.create_action_plan() 
 
@@ -52,7 +53,7 @@ fused_plan = fused_schedule.create_action_plan()
 
 fused_plan.unroll(ii)
 fused_plan.vectorize(j)
-# fused_plan.unroll(k)
+fused_plan.unroll(k)
 
 
 package = acc.Package()
