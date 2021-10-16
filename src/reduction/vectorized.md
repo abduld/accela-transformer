a3602094fb47a9bb5a2e5f5572977cc73b8f3d41
abdul dakkak <abduld@wolfram.com>
2021-10-16 20:49:18 +0000

    checkpoint

---
+++ src/reduction/vectorized.md
@@ -0,0 +1,17 @@
+# Vectorized Accera Implementation
+
+
+[](vectorized.py ':include :type=code python :fragment=import-package')
+
+[](vectorized.py ':include :type=code python :fragment=declare-target-dependent-properties')
+
+
+[`<link href="foo">` html]
+
+
+<pre><code class="language-css">p { color: red }</code></pre>
+
+
+```python
+import robopy as acc
+```
--- src/reduction/vectorized.py
+++ src/reduction/vectorized.py
@@ -1,58 +1,64 @@
 #!/usr/bin/env python3
 
-import math
+### [import-package]
 import robopy as acc
+### [import-package]
+ 
 
-N = 2 ** 20
-DEV_MODE = False
-
+### [declare-target-dependent-properties]
 target = acc.Target(category=acc.Target.Category.CPU)
 vector_size = (
     target.vector_bytes // 4
 )  # AVX-2 gives 256-bit registers, which can hold 8 floats
 vector_units = 2 * vector_size  # AVX-2 has 16 256-bit registers
-split_size =  4* vector_size
+split_size = 4 * vector_size
+### [declare-target-dependent-properties]
+ 
+N = 2 ** 20 
 
 Input = acc.Array(
     role=acc.Array.Role.INPUT, element_type=acc.ScalarType.float32, shape=(N,)
 )
 SumVec = acc.Array(
     role=acc.Array.Role.TEMP, element_type=acc.ScalarType.float32, shape=(split_size,)
-) 
+)
 Sum = acc.Array(
     role=acc.Array.Role.INPUT_OUTPUT, element_type=acc.ScalarType.float32, shape=(1,)
-) 
- 
+)
+
 vec_accum_nest = acc.Nest(shape=(N // split_size, split_size))
 i, j = vec_accum_nest.get_indices()
+
 
 @vec_accum_nest.iteration_logic
 def _():
     SumVec[j] += Input[i * split_size + j]
 
- 
+
 finalize_accum_nest = acc.Nest(shape=(split_size,))
 k = finalize_accum_nest.get_indices()
+
+
 @finalize_accum_nest.iteration_logic
 def _():
     Sum[0] += SumVec[k]
+
 
 vec_accum_schedule = vec_accum_nest.create_schedule()
 finalize_accum_schedule = finalize_accum_nest.create_schedule()
 
 fused_schedule = acc.fuse((vec_accum_schedule, finalize_accum_schedule), partial=0)
 f, i, j, k = fused_schedule.get_indices()
- 
-ii = fused_schedule.split(i, vector_units) 
+
+ii = fused_schedule.split(i, vector_units)
 
 # fused_schedule.reorder((f, i, ii, j, k))
 
-fused_plan = fused_schedule.create_action_plan() 
-
+fused_plan = fused_schedule.create_action_plan()
 
 
 fused_plan.unroll(ii)
-fused_plan.vectorize(j) 
+fused_plan.vectorize(j)
 fused_plan.unroll(k)
 
 
